98c4941fe45139b553b9042bb886f99938f28db6
wailovet <wailovet@163.com>
2024-01-10 01:08:44 +0800

    'fix'

--- __init__.py
+++ __init__.py
@@ -11,12 +11,24 @@ def install_package(package_name):
 
 
 package_list = ["imageio", "cupy", "imageio_ffmpeg", 'cv2']  # Replace with your list of packages
-install_name = ['imageio', 'cupy-cuda12x', 'imageio[ffmpeg]', 'opencv-python-headless']
+install_name = ['imageio', '', 'imageio[ffmpeg]', 'opencv-python-headless']
 
 for package,name in zip(package_list,install_name):
     try:
         __import__(package)
     except ImportError:
+        if package == "cupy":
+            cuda_version = int(round(float(torch.version.cuda)*10))
+            if cuda_version <= 102:
+                name = "cupy-cuda102"
+            elif cuda_version <= 110:
+                name = "cupy-cuda110"
+            elif cuda_version <= 111:
+                name = "cupy-cuda111"
+            elif cuda_version <= 118:
+                name = "cupy-cuda11x"
+            else:
+                name = "cupy-cuda12x" 
         install_package(name)
 
 from .masknumcap import MaskListcaptoBatch
